d126f77a4a55e615053a9ef6709de10594a1df23
EOS Chrome <40155368+eosCHROME@users.noreply.github.com>
2018-10-13 21:03:43 +0900

    Update get_json_api.cpp

--- src/get_json_api.cpp
+++ src/get_json_api.cpp
@@ -1,15 +1,27 @@
+#include <cpprest/http_client.h>
+#include <cpprest/filestream.h>
 #include <iostream>
-#include <push_data.hpp>
+#include <cpprest/json.h>
+using namespace utility;                    // Common utilities like string conversions
+using namespace web;                        // Common features like URIs.
+using namespace web::http;                  // Common HTTP functionality
+using namespace web::http::client;          // HTTP client features
+using namespace concurrency::streams;       // Asynchronous streams
 
+void getJsonAPI()
+{
+	http_client client2(U("http://api.krw.ximaxgames.com/wallet/api/getAccountBalance?isRenew=true&operatorID=ibct_iwc&thirdPartyCode=29&time=1539427068325&userID=IBCT_IWC&vendorID=0&hash=bfb69d8998ad6ab58419c1d1dd0c23b7"));
+	http_request req2(methods::GET);
+client2.request(req2).then([=](http_response r){
+//  web::json::value v1 = json::value::parse(U("{ \"a\" : 10 }"));
+//  std::cout << v1.at(U("a")) << std::endl;
+//  std::cout << v1 << std::endl;
+  std::cout << std::endl<< std::endl << r.extract_json().get() << std::endl << std::endl;
+}).wait();
+}
 
-  void uploadToBlockchain(std::string);
-  std::string getTransactionNumber(); //get this directly when push transaction to the uploadToBlockchain
-  std::string getTransactionHash();
-  std::string mergeData(std::string trx, std::string hash, std::string api);
-  void createJSON(std::string mergeddata);
-  void uploadToDB(std::string filename);
-
-  int main() {
+int main(int argc, char* argv[])
+{
+    getJsonAPI();
     return 0;
-
-  }
+}
