7d9075122f46bf8e1e5794e69674684dcdae4577
EOS Chrome <40155368+eosCHROME@users.noreply.github.com>
2018-10-13 21:05:04 +0900

    Delete define.h

--- define.h
+++
@@ -1,5 +0,0 @@
-#define ID "ibct_iwc"
-#define NAME "IBCT"
-#define EMAIL "lucas@fdsigaming.com"
-#define API_URL "http://api.krw.ximaxgames.com/api"
-#define PRIVATE_KEY "2A8B23B1B2C1DD6C09733F8E6C59D037"
