6be63a86ddf765d14ec3dc64586e57183f2f5764
EOS Chrome <40155368+eosCHROME@users.noreply.github.com>
2018-10-17 11:34:13 +0900

    Create define.h

---
+++ src/define.h
@@ -0,0 +1,7 @@
+#define ID "ibct_iwc"
+#define NAME "IBCT"
+#define EMAIL "lucas@fdsigaming.com"
+#define API_URL "http://api.krw.ximaxgames.com/wallet/api/searchTransactions?"
+#define PRIVATE_KEY "2A8B23B1B2C1DD6C09733F8E6C59D037"
+#define START_DATE "2018-10-01"
+#define START_TIME "00:00:00"
